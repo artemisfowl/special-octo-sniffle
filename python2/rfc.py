11eeed2fcc002e9badd1f2a0a6257dd7d2c00deb
root <root@centbox.(none)>
2018-08-30 23:00:40 +0530

    Removed redundant code

--- python2/rfc.py
+++ python2/rfc.py
@@ -164,23 +164,5 @@ def main():
 	print('\nParsing the information from the local file')
 	parse_rfc_ref(RFC_LOCAL_REF)
 
-	#for line in inp(['./details_rfc']):
-		#if 'RFC' in line.strip():
-			#mkey_list = line.strip().split('|')
-			#print(len(mkey_list))
-			#for i in xrange(0, len(mkey_list)):
-				#if i == 0:
-					## this is the name of the RFC
-					#print(mkey_list[i], end = '\t')
-				#elif i == len(mkey_list) -1:
-					# this is the one with the extra amount
-					# of data
-					#last_data = (mkey_list[i].strip())
-
-					## now get the title of the RFC
-					#print(last_data[last_data.index('"') +
-						#1 : last_data.rfind('",')])
-			#break
-
 if __name__ == '__main__':
 	main()
