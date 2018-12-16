6c9181c6779bdc7d202f6eda885afa82ba66e5cb
artemisfowl <skb655952@gmail.com>
2018-12-16 14:17:16 +0530

    Added nitwise calculation code

--- asm/main.asm
+++ asm/main.asm
@@ -2,20 +2,58 @@
 ; @author sb
 ; @brief testing mode for assembly language programming
 
+section .data
+EXIT_SUCCESS equ 0					; successful operation
+EXIT_FAILURE equ 1					; erroneous operation
+SYS_EXIT equ 60							; call code to terminate
 
+; 8 bit variable space (byte)
+bVar1 db 17
+bVar2 db 9
+bResult db 0
+
+; 16 bit variable space (word)
+wVar1 dw 17000
+wVar2 dw 9000
+wResult dw 0
+
+; 32 bit variable space (double word)
+dVar1 dd 17000000
+dVar2 dd 9000000
+dResult dd 0
+
+; 64 bit variable space (quad word)
+qVar1 dq 170000000
+qVar2 dq 90000000
+qResult dq 0
+
+; code section
 section .text
-global _start									; let the assembler know the start of the program
+global _start
 
 _start:
-	mov edx, len								; length of the message
-	mov ecx, msg								; message to be written
-	mov ebx, 1									; provide the file descriptor(stdout in this case)
-	mov eax, 4									; system call number(sys_write)
-	int 80h											; interrupt and call the kernel
+	; 8 bit addition
+	mov al, byte [bVar1]
+	add al, byte [bVar2]
+	mov byte [bResult], al
 
-	mov eax, 1									; system call number(sys_exit)
-	int 80h											; interrupt and call the kernel
+	; 16 bit addition
+	mov ax, word [wVar1]
+	add ax, word [wVar2]
+	mov word [wResult], ax
 
-section .data
-msg db 'Working!', 0ah				; string to be printed
-len equ $ - msg 							; length of the message
+	; 32 bit addition
+	mov eax, dword [dVar1]
+	add eax, dword [dVar2]
+	mov dword[dResult], eax
+
+	; 64 bit addition
+	mov rax, qword [qVar1]
+	add rax, qword [qVar2]
+	mov qword [qResult], rax
+
+; return code for the execution of the program
+last:
+	mov rax, SYS_EXIT					; call code for exit
+	mov rdi, EXIT_SUCCESS 		; exit the program with success
+	syscall
